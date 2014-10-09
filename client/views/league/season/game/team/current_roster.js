e55402bc6005fe2c113953763cd5056d30d80d53
Philip Howley <howley.phil@gmail.com>
2014-10-09 13:16:35 -0700

    built main structure from league to player

---
+++ client/views/league/season/game/team/current_roster.js
@@ -0,0 +1,50 @@
+Template.tRoster.cRoster = function() {
+  return Players.find({
+    gameStatus: {
+      $in: ["starting", "sub"]
+    }
+  }, {
+    sort: {
+      firstName: 1
+    }
+  });
+};
+Template.tRoster.cStarters = function() {
+  return Players.find({
+    gameStatus: {
+      $in: ["starting"]
+    }
+  }, {
+    sort: {
+      firstName: 1
+    }
+  });
+};
+
+Template.tRoster.sPlayerId = function() {
+  return Session.get('sPlayerId');
+};
+
+Template.tRoster.playersOff = function() {
+  return Players.find({
+    gameStatus: "out"
+  }, {
+    sort: {
+      firstName: 1
+    }
+  });
+};
+
+Template.tRoster.helpers({
+
+  gameDayRosterCount: function() {
+    return Players.find({
+      gameStatus: {
+        $in: ["starting", "sub"]
+      }
+    }).count();
+  },
+  totalRoster: function() {
+    return Players.find().count();
+  }
+});
