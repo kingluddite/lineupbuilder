82285fe4332390809a6cbaefed30b6f2a817bf6f
Philip Howley <howley.phil@gmail.com>
2014-12-01 21:21:20 -0800

    made teams appear under season

--- client/views/team/add_team.js
+++ client/views/team/add_team.js
@@ -1,42 +1,43 @@
 Template.tAddTeam.helpers({
-  sTeamId: function() {
-    return Session.get('sTeamId');
-  },
-  sEditMode: function() {
-    return Session.get('sEditMode');
-  }
+    sTeamId: function() {
+        return Session.get('sTeamId');
+    },
+    sEditMode: function() {
+        return Session.get('sEditMode');
+    }
 });
 
 // adding events to our templates (duh!)
 Template.tAddTeam.events({
-  'click .add': function() {
-    // makes focus on first form work as it should
-    setTimeout(function() {
-      $('input[name="teamName"]').focus();
-    }, 500);
-    Session.set('sEditMode', true);
-  },
+    'click .add': function() {
+        // makes focus on first form work as it should
+        setTimeout(function() {
+            $('input[name="teamName"]').focus();
+        }, 500);
+        Session.set('sEditMode', true);
+    },
 
-  'click .remove': function() {
-    Session.set('sEditMode', false);
-  },
+    'click .cancel': function() {
+        console.log('yo');
+        Session.set('sEditMode', false);
+    },
 
-  'submit form': function(evt) {
-    evt.preventDefault();
+    'submit form': function(evt) {
+        evt.preventDefault();
 
-    var team = {
-      teamName: $(evt.target).find('[name=teamName]').val(),
-      leagueId: $(evt.target).find('[name=teamName]').val()
-    };
+        var team = {
+            teamName: $(evt.target).find('[name=teamName]').val(),
+            leagueId: $(evt.target).find('[name=teamName]').val()
+        };
 
-    Meteor.call('addTeam', team, function(error) {
-      if (error) {
-        // return alert(error.reason);
-        return throwError(error.reason);
-      }
-      // Router.go('playerPage', {_id: id});
-    });
+        Meteor.call('addTeam', team, function(error) {
+            if (error) {
+                // return alert(error.reason);
+                return throwError(error.reason);
+            }
+            // Router.go('playerPage', {_id: id});
+        });
 
-    Session.set('sEditMode', false);
-  }
+        Session.set('sEditMode', false);
+    }
 });
